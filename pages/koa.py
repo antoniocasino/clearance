5c5bee5ced816e98f81874b74f75089f951f4010
antoniocasino <casinoantonio@gmail.com>
2025-02-18 10:31:25 +0000

    fix range

--- pages/koa.py
+++ pages/koa.py
@@ -13,43 +13,31 @@ def koa_page():
     )   
     
     qb = st.number_input(
-        "qb",
-        min_value=0.01,
-        max_value=1.0,        
-        value=0.25,     
-        step=0.01,
-        format="%.2f"
+        "qb",           
+        value=300,     
+        step=1        
     )
     qd = st.number_input(
-        "qd",
-        min_value=0.01,
-        max_value=1.0,           
-        value=0.50,     
-        step=0.01,
-        format="%.2f"
+        "qd",              
+        value=500,     
+        step=1        
     )
     qf = st.number_input(
-        "qf",
-        min_value=0.01,
-        max_value=1.0,       
-        value=0.75,     
-        step=0.01,
-        format="%.2f"
+        "qf",        
+        value=10,     
+        step=1        
     )
     kd = st.number_input(
-        "kd",
-        min_value=0.01,
-        max_value=1.0,       
-        value=0.1,     
-        step=0.01,
-        format="%.2f"
+        "kd",            
+        value=250,     
+        step=1        
     )    
     koa_button = st.button(key="koa", label="Calculate")
 
     if koa_button:
         with st.spinner("Extracting... it takes time..."): 
     
-            kdif_result, koa_result = koa(qd,qb,qf,kd)            
+            kdif_result, koa_result = koa(qb=qb,qd=qd,qf=qf,kd=kd)
             if kdif_result is not None and koa_result is not None:
-                st.write(f"Kdif: {round(kdif_result, 2)}")
-                st.write(f"KoA: {round(koa_result, 2)}")
+                st.write(f"Kdif: {round(kdif_result, 1)}")
+                st.write(f"KoA: {round(koa_result, 1)}")
